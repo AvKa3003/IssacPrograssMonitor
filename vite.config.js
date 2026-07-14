bf1f3e3ffd9aba86552dbc4680ce382b2122d7a1
AvKa3003 <kirillka3003@gmail.com>
2026-07-14 23:10:35 +0300

    gh actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Для GitHub Pages замени 'issacProgress' на имя своего репозитория
+// GitHub Pages: https://avka3003.github.io/IssacPrograssMonitor/
 export default defineConfig({
   plugins: [vue()],
-  base: '/issacProgress/',
+  base: '/IssacPrograssMonitor/',
 })
